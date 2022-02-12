946e4e58b2181b2c4e31fd7019a3eab8a6916023
unknown <felicien.haraux@hotmail.fr>
2022-02-12 18:18:17 +0100

    init comit

---
+++ src/main/java/com/gestionnairetache/GestionnaireTacheApplication.java
@@ -0,0 +1,13 @@
+package com.gestionnairetache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireTacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionnaireTacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionnairetache/GestionnaireTacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionnairetache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireTacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
