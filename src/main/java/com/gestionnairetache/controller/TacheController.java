0a8040c52b2317c53b28409c15003fa11f2f008f
unknown <felicien.haraux@hotmail.fr>
2022-03-31 16:32:51 +0200

    correction cors error

--- src/main/java/com/gestionnairetache/controller/TacheController.java
+++ src/main/java/com/gestionnairetache/controller/TacheController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.origin.Origin;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/taches")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TacheController {
 
     @Autowired
